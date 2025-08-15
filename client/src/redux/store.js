0be7b25059a4b77c9d8f67edbdee3f70bca21a42
arroo <Pm9521263317@gmail.com>
2025-08-15 17:46:07 +0530

    `Removed unused code in store.js`

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,16 +4,7 @@ import authReducer from "./features/auth/authSlice";
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    // Add other reducers here
   },
-  // middleware: (getDefaultMiddleware) =>
-  //   getDefaultMiddleware({
-  //     serializableCheck: {
-  //       ignoredActions: ["auth/loginSuccess"],
-  //       ignoredPaths: ["auth.user"],
-  //     },
-  //   }),
-  // devTools: process.env.NODE_ENV !== "production",
 });
 
 export default store;
